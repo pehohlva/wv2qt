16fc792a1d9dd4de9c7390a13a2b53dd152f1774
pehohlva <dev@macpro.local>
2013-12-12 12:03:26 +0100

    init d

---
+++ config.h
@@ -0,0 +1,19 @@
+/* 
+ config to all this file
+ + image handling by qt5
+ */
+
+#ifndef WINPORT_H
+#define	WINPORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+
+
+#endif	/* WINPORT_H */
+
+
