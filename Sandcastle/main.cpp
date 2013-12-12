8432a35ad4f2ad2aa235accb6e9f44df0b483af9
pehohlva <pehohlva@gmail.com>
2013-12-12 21:05:02 +0100

    Update main.cpp

--- Sandcastle/main.cpp
+++ Sandcastle/main.cpp
@@ -20,14 +20,14 @@ int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
     QTextStream out(stdout);
     QString str("x");
-    out <<  str.fill("*",100) << "\n";
+    out <<  str.fill('*',100) << "\n";
     const QString qtfileword = QString("testole.doc");
     DocModelRead *doc = new DocModelRead("utf-8");
     doc->readDocument(qtfileword);
     const QString xml_LONG = ReaderSession::self()->doc().toString(5); 
     ReaderSession::self()->nextOle(); 
     out << xml_LONG << "\n";
-    out <<  str.fill("*",100) << "\n";
+    out <<  str.fill('*',100) << "\n";
     out.flush();
     QTimer::singleShot(4000, &a, SLOT(quit()));
     return a.exec();
